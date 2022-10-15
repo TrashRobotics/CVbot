3444b5d709f5e79b18ebb1fb31e581b6e10b53e0
Frank052100 <xufaen999@gmail.com>
2022-10-15 11:19:43 +0800

    chinese ver.2

--- arduino_code/cv_bot/cv_bot.ino
+++ arduino_code/cv_bot/cv_bot.ino
@@ -1,69 +1,74 @@
 #include "ArduinoJson.h"
-#define IN1_PIN   5   // пины драйвера двигателей MX1508
-#define IN2_PIN   6   //
-#define IN3_PIN   9   //
-#define IN4_PIN   10  //  
-#define LED_PIN   13  
+#define IN1_PIN 5  // MX1508 電機驅動引腳
+#define IN2_PIN 6  //
+#define IN3_PIN 9  //
+#define IN4_PIN 10 //
+#define LED_PIN 13
 
-// json буфер для пакетов от orange pi
+// 來自橙色 pi 的數據包的 json 緩衝區
 StaticJsonDocument<200> jsondoc;
 
-// глобальные переменные скорости
+// 全局速度變量
 int16_t speedA = 0;
 int16_t speedB = 0;
-
 
 void setup()
 {
   Serial.begin(9600);
-  pinMode(IN1_PIN, OUTPUT);    
-  pinMode(IN2_PIN, OUTPUT);    
-  pinMode(IN3_PIN, OUTPUT);    
-  pinMode(IN4_PIN, OUTPUT);    
+  pinMode(IN1_PIN, OUTPUT);
+  pinMode(IN2_PIN, OUTPUT);
+  pinMode(IN3_PIN, OUTPUT);
+  pinMode(IN4_PIN, OUTPUT);
   pinMode(LED_PIN, OUTPUT);
 }
 
 void loop()
 {
-  DeserializationError err = deserializeJson(jsondoc, Serial);  // получаем сообщение от orange pi через uart
-  if (err == DeserializationError::Ok)    // если cообщение принято
+  DeserializationError err = deserializeJson(jsondoc, Serial); // 通過 uart 從橙色 pi 接收消息
+  if (err == DeserializationError::Ok)                         // 如果收到消息
   {
-    speedA = (float)jsondoc["speedA"] * 2.55;  // приходит диапазон [-100; 100]
-    speedB = (float)jsondoc["speedB"] * 2.55;  // расширяем до [-255; 255]
+    speedA = (float)jsondoc["speedA"] * 2.55; // 範圍 [-100; 100]
+    speedB = (float)jsondoc["speedB"] * 2.55; // 擴展到 [-255; 255]
   }
   else
   {
-    while (Serial.available() > 0) Serial.read(); // чистим буфер
+    while (Serial.available() > 0)
+      Serial.read(); // 清除緩衝區
   }
-  moveA(speedA);  // обновляем состояние скоростей моторов
-  moveB(speedB);  //
+  moveA(speedA); // 更新電機速度狀態
+  moveB(speedB); //
   delay(10);
 }
 
-void moveA(int16_t speed){
-  //speed = -speed;
-  if(speed >= 0){
-    analogWrite(IN1_PIN, LOW);  
+void moveA(int16_t speed)
+{
+  // speed = -speed;
+  if (speed >= 0)
+  {
+    analogWrite(IN1_PIN, LOW);
     analogWrite(IN2_PIN, abs(speed));
     digitalWrite(LED_BUILTIN, LOW);
-  } 
-  else {
-    analogWrite(IN1_PIN, abs(speed));  
+  }
+  else
+  {
+    analogWrite(IN1_PIN, abs(speed));
     analogWrite(IN2_PIN, LOW);
     digitalWrite(LED_BUILTIN, HIGH);
   }
 }
 
-void moveB(int16_t speed){
-  if(speed >= 0){
-    analogWrite(IN3_PIN, LOW);  
+void moveB(int16_t speed)
+{
+  if (speed >= 0)
+  {
+    analogWrite(IN3_PIN, LOW);
     analogWrite(IN4_PIN, abs(speed));
     digitalWrite(LED_BUILTIN, LOW);
-  } 
-  else {
-    analogWrite(IN3_PIN, abs(speed));  
+  }
+  else
+  {
+    analogWrite(IN3_PIN, abs(speed));
     analogWrite(IN4_PIN, LOW);
     digitalWrite(LED_BUILTIN, HIGH);
   }
 }
- 
--- python_app/app.py
+++ python_app/app.py
@@ -7,20 +7,21 @@ import json
 import argparse
 
 app = Flask(__name__)
-camera = cv2.VideoCapture(0)  # веб камера
+camera = cv2.VideoCapture(0)  # camera
 
-controlX, controlY = 0, 0  # глобальные переменные положения джойстика с web-страницы
+controlX, controlY = 0, 0  # global joystick position variables from the web page
 
 
 def getFramesGenerator():
-    """ Генератор фреймов для вывода в веб-страницу, тут же можно поиграть с openCV"""
+    """ 用於輸出到網頁的幀生成器，您可以在此處使用 openCV"""
     while True:
-        time.sleep(0.01)    # ограничение fps (если видео тупит, можно убрать)
-        success, frame = camera.read()  # Получаем фрейм с камеры
+        time.sleep(0.01)    # fps 限制（如果視頻很愚蠢，您可以將其刪除）
+        success, frame = camera.read()  # 從相機獲取幀
         if success:
-            frame = cv2.resize(frame, (320, 240), interpolation=cv2.INTER_AREA)  # уменьшаем разрешение кадров (если видео тупит, можно уменьшить еще больше)
-            # frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)   # перевод изображения в градации серого
-            # _, frame = cv2.threshold(frame, 127, 255, cv2.THRESH_BINARY)  # бинаризуем изображение
+            # 降低幀分辨率（如果視頻很暗淡，可以進一步降低）
+            frame = cv2.resize(frame, (320, 240), interpolation=cv2.INTER_AREA)
+            # frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) # 將圖像轉換為灰度
+            # _, frame = cv2.threshold(frame, 127, 255, cv2.THRESH_BINARY)  # binarize the image
             _, buffer = cv2.imencode('.jpg', frame)
             yield (b'--frame\r\n'
                    b'Content-Type: image/jpeg\r\n\r\n' + buffer.tobytes() + b'\r\n')
@@ -28,59 +29,68 @@ def getFramesGenerator():
 
 @app.route('/video_feed')
 def video_feed():
-    """ Генерируем и отправляем изображения с камеры"""
+    """We generate and send images from the camera"""
     return Response(getFramesGenerator(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 
 @app.route('/')
 def index():
-    """ Крутим html страницу """
+    """ I am creating a html page """
     return render_template('index.html')
 
 
 @app.route('/control')
 def control():
-    """ Пришел запрос на управления роботом """
+    """ Received a request to control the robot """
     global controlX, controlY
-    controlX, controlY = float(request.args.get('x')) / 100.0, float(request.args.get('y')) / 100.0
+    controlX, controlY = float(request.args.get(
+        'x')) / 100.0, float(request.args.get('y')) / 100.0
     return '', 200, {'Content-Type': 'text/plain'}
 
 
 if __name__ == '__main__':
-    # пакет, посылаемый на робота
+    # 發送給機器人的數據包
     msg = {
-        "speedA": 0,  # в пакете посылается скорость на левый и правый борт тележки
+        "speedA": 0,  # 數據包將速度發送到轉向架的左右兩側
         "speedB": 0  #
     }
 
-    # параметры робота
-    speedScale = 0.65  # определяет скорость в процентах (0.50 = 50%) от максимальной абсолютной
-    maxAbsSpeed = 100  # максимальное абсолютное отправляемое значение скорости
-    sendFreq = 10  # слать 10 пакетов в секунду
+    # 機器人參數
+    speedScale = 0.65  # 將速度定義為最大絕對值的百分比 (0.50 = 50%)
+    maxAbsSpeed = 100  # 要發送的最大絕對速度值
+    sendFreq = 10  # 每秒發送 10 個數據包
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('-p', '--port', type=int, default=5000, help="Running port")
-    parser.add_argument("-i", "--ip", type=str, default='127.0.0.1', help="Ip address")
-    parser.add_argument('-s', '--serial', type=str, default='/dev/ttyUSB0', help="Serial port")
+    parser.add_argument('-p', '--port', type=int,
+                        default=5000, help="Running port")
+    parser.add_argument("-i", "--ip", type=str,
+                        default='127.0.0.1', help="Ip address")
+    parser.add_argument('-s', '--serial', type=str,
+                        default='/dev/ttyUSB0', help="Serial port")
     args = parser.parse_args()
 
-    serialPort = serial.Serial(args.serial, 9600)   # открываем uart
+    serialPort = serial.Serial(args.serial, 9600)   # 打開uart
 
     def sender():
-        """ функция цикличной отправки пакетов по uart """
+        """ uart循環發送數據包功能 """
         global controlX, controlY
         while True:
-            speedA = maxAbsSpeed * (controlY + controlX)    # преобразуем скорость робота,
-            speedB = maxAbsSpeed * (controlY - controlX)    # в зависимости от положения джойстика
+            speedA = maxAbsSpeed * (controlY + controlX)    # 轉換機器人的速度
+            speedB = maxAbsSpeed * (controlY - controlX)    # 取決於操縱桿的位置
 
-            speedA = max(-maxAbsSpeed, min(speedA, maxAbsSpeed))    # функция аналогичная constrain в arduino
-            speedB = max(-maxAbsSpeed, min(speedB, maxAbsSpeed))    # функция аналогичная constrain в arduino
+            # 功能類似於arduino中的約束
+            speedA = max(-maxAbsSpeed, min(speedA, maxAbsSpeed))
+            # 功能類似於arduino中的約束
+            speedB = max(-maxAbsSpeed, min(speedB, maxAbsSpeed))
 
-            msg["speedA"], msg["speedB"] = speedScale * speedA, speedScale * speedB     # урезаем скорость и упаковываем
+            msg["speedA"], msg["speedB"] = speedScale * \
+                speedA, speedScale * speedB     # 降低速度和包裝
 
-            serialPort.write(json.dumps(msg, ensure_ascii=False).encode("utf8"))  # отправляем пакет в виде json файла
+            serialPort.write(json.dumps(msg, ensure_ascii=False).encode(
+                "utf8"))  # 將包作為 json 文件發送
             time.sleep(1 / sendFreq)
 
-    threading.Thread(target=sender, daemon=True).start()    # запускаем тред отправки пакетов по uart с демоном
+    # 使用守護進程啟動一個線程以通過 uart 發送數據包
+    threading.Thread(target=sender, daemon=True).start()
 
-    app.run(debug=False, host=args.ip, port=args.port)   # запускаем flask приложение
+    app.run(debug=False, host=args.ip, port=args.port)   # 運行燒瓶應用程序
